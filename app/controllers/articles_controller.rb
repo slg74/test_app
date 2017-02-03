da5a0eb96b7e9c7ec097b26ed35734c38a72f519
scott <scott.gillespie@r1soft.com>
2017-02-03 05:43:34 -0600

    done w/ tutorial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with_name: "scott", password: "secret", except:
+  [:index, :show]
+
   def index
     @articles = Article.all
   end
