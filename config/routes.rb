1283e7025ecdbdf8adaff75cd5712e481c9d79b5
scott <scott.gillespie@r1soft.com>
2017-02-02 21:01:03 -0600

    complete rails test app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  resources :articles
+
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -54,4 +58,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root 'welcome#index'
 end
